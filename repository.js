21d40731197be5adcb4d1cdfd71a34ff8f1d34e3
Rodrigo Roa Rodríguez <rodrigoroarod@gmail.com>
2017-02-09 12:08:51 +0100

    Remove repository file.

--- repository.js
+++
@@ -1,7 +0,0 @@
-// Lets create a group that is centered in our SVG canvas
-let svg.g = svg.d3selection // Selects the svg in our DOM.
-  .append('g')  // Appends a g tag: <svg ... ><g><g></svg>
-  // N.B!!! our selection is now the g tag NOT the svg tag.
-  .attr('transform', `translate(0, ${svg.height / 2})`)
-  //Tranforms applies a transform matrix on the group
-  //Tranforms is SVG stuff not D3 see: https://developer.mozilla.org/en/docs/Web/SVG/Attribute/transform
